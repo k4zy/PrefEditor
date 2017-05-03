fa1f4f0ec7382825a5640531995838def3bba9f7
kazuki-yoshida <kzk.yshd@gmail.com>
2017-05-03 23:33:54 +0900

    Fragmentation

--- prefeditor/src/main/java/com/github/kazy1991/prefeditor/view/PrefEditorActivity.kt
+++ prefeditor/src/main/java/com/github/kazy1991/prefeditor/view/PrefEditorActivity.kt
@@ -6,31 +6,16 @@ import android.support.v4.view.MenuItemCompat
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.SearchView
 import android.view.Menu
-import android.widget.Spinner
 import com.github.kazy1991.prefeditor.R
-import com.github.kazy1991.prefeditor.adapter.SchemaSpinnerAdapter
-import com.github.kazy1991.prefeditor.callback.SearchViewWrapperCallback
-import com.github.kazy1991.prefeditor.callback.SpinnerWrapperCallback
-import com.github.kazy1991.prefeditor.model.PrefItem
-import com.github.kazy1991.prefeditor.presenter.PrefEditorPresenter
-import com.github.kazy1991.prefeditor.repository.FileSystemPrefListRepository
-import com.github.kazy1991.prefeditor.usecase.PrefEditorUseCaseImpl
 
-class PrefEditorActivity : AppCompatActivity(), PrefEditorView {
-
-    val spinner by lazy { findViewById(R.id.spinner) as Spinner }
-
-    lateinit var presenter: PrefEditorPresenter
+class PrefEditorActivity : AppCompatActivity() {
 
     var searchView: SearchView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pref_editor)
-        // todo: DI
-        val prefListRepository = FileSystemPrefListRepository(this)
-        val prefEditorUseCase = PrefEditorUseCaseImpl(this, prefListRepository)
-        presenter = PrefEditorPresenter(this, prefEditorUseCase)
+        replaceFragment(PrefEditorFragment.newInstance())
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -40,13 +25,13 @@ class PrefEditorActivity : AppCompatActivity(), PrefEditorView {
         searchView = (MenuItemCompat.getActionView(menuItem) as SearchView).apply {
             setIconifiedByDefault(true)
             isSubmitButtonEnabled = false
-            setOnSearchClickListener { presenter.startSearch() }
-            setOnQueryTextListener(object : SearchViewWrapperCallback {
-                override fun onTextChange(newText: String): Boolean {
-                    presenter.onSearchTextChanged(newText)
-                    return false
-                }
-            })
+//            setOnSearchClickListener { presenter.startSearch() }
+//            setOnQueryTextListener(object : SearchViewWrapperCallback {
+//                override fun onTextChange(newText: String): Boolean {
+//                    presenter.onSearchTextChanged(newText)
+//                    return false
+//                }
+//            })
         }
         return super.onCreateOptionsMenu(menu)
     }
@@ -55,19 +40,6 @@ class PrefEditorActivity : AppCompatActivity(), PrefEditorView {
         supportFragmentManager.beginTransaction()
                 .replace(R.id.content_frame, fragment)
                 .commit()
-    }
-
-    override fun updatePrefNameList(list: List<PrefItem>) {
-        spinner.adapter = SchemaSpinnerAdapter(this, list)
-        spinner.onItemSelectedListener = object : SpinnerWrapperCallback {
-            override fun onItemSelected(item: PrefItem) {
-                presenter.onItemTapped(item)
-            }
-        }
-    }
-
-    override fun replacePrefView(prefName: String) {
-        replaceFragment(PrefListFragment.newInstance(prefName))
     }
 
 }
---
+++ prefeditor/src/main/java/com/github/kazy1991/prefeditor/view/PrefEditorFragment.kt
@@ -0,0 +1,59 @@
+package com.github.kazy1991.prefeditor.view
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Spinner
+import com.github.kazy1991.prefeditor.R
+import com.github.kazy1991.prefeditor.adapter.SchemaSpinnerAdapter
+import com.github.kazy1991.prefeditor.callback.SpinnerWrapperCallback
+import com.github.kazy1991.prefeditor.model.PrefItem
+import com.github.kazy1991.prefeditor.presenter.PrefEditorPresenter
+import com.github.kazy1991.prefeditor.repository.FileSystemPrefListRepository
+import com.github.kazy1991.prefeditor.usecase.PrefEditorUseCaseImpl
+
+class PrefEditorFragment : Fragment(), PrefEditorView {
+
+    val spinner by lazy { view?.findViewById(R.id.spinner) as Spinner }
+
+    lateinit var presenter: PrefEditorPresenter
+
+    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater?.inflate(R.layout.fragment_pref_editor, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        // todo: DI
+        val prefListRepository = FileSystemPrefListRepository(context)
+        val prefEditorUseCase = PrefEditorUseCaseImpl(context, prefListRepository)
+        presenter = PrefEditorPresenter(this, prefEditorUseCase)
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        childFragmentManager.beginTransaction()
+                .replace(R.id.content_frame, fragment)
+                .commit()
+    }
+
+    override fun updatePrefNameList(list: List<PrefItem>) {
+        spinner.adapter = SchemaSpinnerAdapter(context, list)
+        spinner.onItemSelectedListener = object : SpinnerWrapperCallback {
+            override fun onItemSelected(item: PrefItem) {
+                presenter.onItemTapped(item)
+            }
+        }
+    }
+
+    override fun replacePrefView(prefName: String) {
+        replaceFragment(PrefListFragment.newInstance(prefName))
+    }
+
+    companion object {
+        fun newInstance(): Fragment {
+            return PrefEditorFragment()
+        }
+    }
+}
