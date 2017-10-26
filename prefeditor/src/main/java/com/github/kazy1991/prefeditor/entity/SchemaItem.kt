e11864108b1b17ada4262f39391790730e9c1277
kazuki-yoshida <kzk.yshd@gmail.com>
2017-10-27 00:19:38 +0900

    Auto generated: SchemaItem#equals

--- prefeditor/src/main/java/com/github/kazy1991/prefeditor/entity/SchemaItem.kt
+++ prefeditor/src/main/java/com/github/kazy1991/prefeditor/entity/SchemaItem.kt
@@ -19,4 +19,23 @@ class SchemaItem {
     fun normalizedName(): String {
         return annotatedName ?: name
     }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as SchemaItem
+
+        if (name != other.name) return false
+        if (annotatedName != other.annotatedName) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = name.hashCode()
+        result = 31 * result + (annotatedName?.hashCode() ?: 0)
+        return result
+    }
+
 }
