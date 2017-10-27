186c4c885ef768d4ac0196db268ac20c3a9c6b44
kazuki-yoshida <kzk.yshd@gmail.com>
2017-10-28 03:25:46 +0900

    ADD PrefListInteractorTest

---
+++ prefeditor/src/test/java/com/github/kazy1991/prefeditor/interactor/PrefListInteractorTest.kt
@@ -0,0 +1,11 @@
+package com.github.kazy1991.prefeditor.interactor
+
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.annotation.Config
+
+@RunWith(RobolectricTestRunner::class)
+@Config(manifest = Config.NONE)
+class PrefListInteractorTest {
+
+}
